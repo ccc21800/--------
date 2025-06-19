cd0ff45e67b2f21e79c2eb2d2fbaa07b6eb77804
ccc21800 <767004052@qq.com>
2025-06-19 21:35:58 +0800

    v0.0.1

--- user.sql
+++ user.sql
@@ -1,4 +1,5 @@
-INSERT INTO `user` (`stuid`, `username`, `password`, `role`) VALUES ('007', '007', '123456', 'user');
-INSERT INTO `user` (`stuid`, `username`, `password`, `role`) VALUES ('2302580079', 'www', '123456', 'admin');
-INSERT INTO `user` (`stuid`, `username`, `password`, `role`) VALUES ('2402580165', 'cjm', '123456', 'admin');
-INSERT INTO `user` (`stuid`, `username`, `password`, `role`) VALUES ('admin001', 'admin', '123456', 'admin');
+INSERT INTO `user` (`stuid`, `username`, `password`, `role`, `integral`, `last_signin`) VALUES ('001', '001', '123456', 'user', NULL, NULL);
+INSERT INTO `user` (`stuid`, `username`, `password`, `role`, `integral`, `last_signin`) VALUES ('007', '007', '123456', 'user', NULL, NULL);
+INSERT INTO `user` (`stuid`, `username`, `password`, `role`, `integral`, `last_signin`) VALUES ('2302580079', 'www', '123456', 'admin', 1010, '2025/6/19');
+INSERT INTO `user` (`stuid`, `username`, `password`, `role`, `integral`, `last_signin`) VALUES ('2402580165', 'cjm', '123456', 'admin', 10, '2025/6/19');
+INSERT INTO `user` (`stuid`, `username`, `password`, `role`, `integral`, `last_signin`) VALUES ('admin001', 'admin', '123456', 'admin', NULL, NULL);
